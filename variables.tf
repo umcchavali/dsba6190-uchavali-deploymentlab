5fe3b1a8917e0814f9d2760d5fbf73f7f25066e7
Uma Chavali <umcchavali@gmail.com>
2024-11-13 19:31:14 -0500

    update-1

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "uma"         
+  default     = "umc"         
 }
 
 variable "class_name" {
