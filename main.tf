a5c7acacfa28917a902194d73f19222956e8c699
Uma Chavali <umcchavali@gmail.com>
2024-11-28 19:15:10 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -76,9 +76,15 @@ resource "azurerm_mssql_database" "db" {
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
-
-
+  sku_name     = "Basic"
 
   tags = local.tags
+}
+
+
+resource "azurerm_mssql_virtual_network_rule" "azsql" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.sql.id
+  subnet_id = azurerm_subnet.snet.id
 }
 
